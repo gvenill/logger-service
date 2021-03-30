bb8cef5cb057d659f530b19b102f0687fea6235d
Sergey Balukhto <sergey.balukhto@auto1.com>
2021-03-30 14:19:04 +0200

    fix: fix typo with IMockDate interface

--- src/utils/mocks/date-factory.mock.ts
+++ src/utils/mocks/date-factory.mock.ts
@@ -1,7 +1,7 @@
-export interface MockDate {}
+export interface IMockDate {}
 
-export function mockDateFactory(date: string): MockDate {
-  class MockDate extends Date implements MockDate {
+export function mockDateFactory(date: string): IMockDate {
+  class MockDate extends Date implements IMockDate {
     constructor() {
       super(date); // add whatever date you'll expect to get
     }
