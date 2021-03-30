cbd28296894fc4b74359b742b238fa4f5dbd7f33
Sergey Balukhto <sergey.balukhto@auto1.com>
2021-03-30 11:30:44 +0200

    feat: create log record dto

---
+++ src/dto/log-record.dto.ts
@@ -0,0 +1,25 @@
+import { LogLevel } from '../constants/log-level';
+
+export class LogRecordDto {
+  logLevel: LogLevel;
+  logger: string;
+  timestamp: Date;
+  message?: string;
+  userAgent?: string;
+  duration?: number;
+  traceId?: string;
+  request?: {
+    url: string;
+    referrer?: string;
+    method: string;
+    statusCode: number;
+    domain: string;
+  };
+  ip?: string;
+  error?: {
+    message: string;
+    stackTrace: string | string[];
+    type: string;
+  };
+  meta?: unknown;
+}
