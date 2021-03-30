8c8868a801b427cc75fe3d2246000934067d6e97
Sergey Balukhto <sergey.balukhto@auto1.com>
2021-03-30 14:26:18 +0200

    refactor: create a library entry point

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
-export const sum = (a: number, b: number) => {
-  if ('development' === process.env.NODE_ENV) {
-    console.log('boop');
-  }
-  return a + b;
-};
+export { LoggerService } from './logger.service';
+export { LoggerProvider } from './logger.provider';
+export { LogRecord } from './interfaces/log-record.interface';
+export { Logger } from './interfaces/logger';
+export { LogFormatter } from './interfaces/log-formatter.interface';
+export { ResponseLogMapProvider } from './interfaces/response-log-map-provider.interface';
+export { LogLevel, LogName } from './constants';
